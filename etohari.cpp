b6b978f4d9032247859ec4dc5bda503dc610fa41
Ha Loc Hai <117820624+powder2005@users.noreply.github.com>
2024-08-19 22:01:43 +0700

    add more content

---
+++ etohari.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+signed main() {
+        
+        cout << 1000;
+
+        for (int i = 1; i <= 1000; i ++) {
+                cout << i << "\n";
+        }
+        
+        return 0;
+}
